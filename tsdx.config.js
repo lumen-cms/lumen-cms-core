3f8cd70c21710c8e0ccc95f2e3b11eca8f637337
Dominic Garms <djgarms@gmail.com>
2020-07-01 11:11:55 +0800

    minor changes

--- tsdx.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rollup (config, options) {
-    console.log(config, options)
-    if (options.entry === 'src/nextjs.tsx') {
-      config.output.file = config.output.file.replace('dist', 'base/dist')
-    }
-    return config
-  }
-}
