84364233e25ecb38f3c75b4b0c6455cc8c024ace
Dominic Garms <djgarms@gmail.com>
2020-09-02 15:46:05 +0800

    fix button props

--- src/components/button/buttonTypes.ts
+++ src/components/button/buttonTypes.ts
@@ -2,5 +2,5 @@ import { ButtonStoryblok } from '../../typings/generated/components-schema'
 
 export type LmButtonProps = {
   content: ButtonStoryblok
-  onClick: () => void
+  onClick?: () => void
 }
