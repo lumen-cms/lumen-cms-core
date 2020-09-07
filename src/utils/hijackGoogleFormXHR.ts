3470b49f2629deff1a8bbc71e5f19367a35c2577
Steffen Baer <stbaer.mail@gmail.com>
2020-09-07 16:21:56 +0800

    added GoogleForms Hook and Story

---
+++ src/utils/hijackGoogleFormXHR.ts
@@ -0,0 +1,27 @@
+export function hijackGoogleFormsXHR(XHR: {
+  prototype: XMLHttpRequest
+  new (): XMLHttpRequest
+  readonly DONE: number
+  readonly HEADERS_RECEIVED: number
+  readonly LOADING: number
+  readonly OPENED: number
+  readonly UNSENT: number
+}) {
+  const { open } = XHR.prototype
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  XHR.prototype.open = function (method, url, async, user, pass) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+    // @ts-ignore
+    this._url = url
+    if (
+      url.indexOf('gstatic.com') !== -1 ||
+      url.indexOf('docs.google.com') !== -1
+    ) {
+      url = `http://googleformrestyler.apixml.net/corsProxy.aspx?base64Url=${btoa(
+        url
+      )}`
+    }
+    open.call(this, method, url, async, user, pass)
+  }
+}
