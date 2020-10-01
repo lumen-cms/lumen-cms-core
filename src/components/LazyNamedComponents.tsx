ca052c6774470f4cf53dab96d69d9cf38ea8d34f
Dominic Garms <djgarms@gmail.com>
2020-10-01 16:41:42 +0800

    finalize lazy

--- src/components/LazyNamedComponents.tsx
+++ src/components/LazyNamedComponents.tsx
@@ -1,85 +1,89 @@
 import dynamic from 'next/dynamic'
-import { LmCoreComponents } from '../utils/config'
 
-LmCoreComponents.chat_whatsapp = dynamic(
-  () => import(/* webpackChunkName: 'popper' */ './chat/WhatsappChatButton')
-)
-LmCoreComponents.list_search_autocomplete = dynamic(
-  () =>
-    import(
-      /* webpackChunkName: 'popper' */ './list-widget/ListSearchAutocomplete'
-    )
-)
-LmCoreComponents.category_box = dynamic(
-  () => import(/* webpackChunkName: 'popper' */ './list-widget/CategoryBox')
-)
-LmCoreComponents.list_search_field = dynamic(
-  () => import(/* webpackChunkName: 'popper' */ './list-widget/ListSearchField')
-)
+export const LmLazyComponents = {
+  instagram_post: dynamic(
+    () =>
+      import(/* webpackChunkName: 'instagram' */ './instagram/InstagramPost')
+  ),
+  instagram_list: dynamic(
+    () =>
+      import(/* webpackChunkName: 'instagram' */ './instagram/InstagramList')
+  ),
+  chat_whatsapp: dynamic(
+    () => import(/* webpackChunkName: 'popper' */ './chat/WhatsappChatButton')
+  ),
+  list_search_autocomplete: dynamic(
+    () =>
+      import(
+        /* webpackChunkName: 'popper' */ './list-widget/ListSearchAutocomplete'
+      )
+  ),
+  category_box: dynamic(
+    () => import(/* webpackChunkName: 'popper' */ './list-widget/CategoryBox')
+  ),
+  list_search_field: dynamic(
+    () =>
+      import(/* webpackChunkName: 'popper' */ './list-widget/ListSearchField')
+  ),
 
-LmCoreComponents.dialog = dynamic(
-  () => import(/* webpackChunkName: 'addon' */ './dialog/Dialog')
-)
+  dialog: dynamic(
+    () => import(/* webpackChunkName: 'addon' */ './dialog/Dialog')
+  ),
 
-LmCoreComponents.section_parallax = dynamic(
-  () => import(/* webpackChunkName: 'addon' */ './section/SectionParallax')
-)
+  section_parallax: dynamic(
+    () => import(/* webpackChunkName: 'addon' */ './section/SectionParallax')
+  ),
+  accordion: dynamic(
+    () => import(/* webpackChunkName: 'addon' */ './accordion/Accordion')
+  ),
+  accordion_item: dynamic(
+    () => import(/* webpackChunkName: 'addon' */ './accordion/AccordionItem')
+  ),
 
-LmCoreComponents.accordion = dynamic(
-  () => import(/* webpackChunkName: 'addon' */ './accordion/Accordion')
-)
-LmCoreComponents.accordion_item = dynamic(
-  () => import(/* webpackChunkName: 'addon' */ './accordion/AccordionItem')
-)
+  snackbar: dynamic(
+    () => import(/* webpackChunkName: 'snackbar' */ './snackbar/Snackbar')
+  ),
+  image_list: dynamic(
+    () => import(/* webpackChunkName: 'slider' */ './image-list/ImageList')
+  ),
+  image_list_item: dynamic(
+    () => import(/* webpackChunkName: 'slider' */ './image-list/ImageListItem')
+  ),
+  slider: dynamic(
+    () => import(/* webpackChunkName: 'slider' */ './slider/Slider')
+  ),
+  section_video_bg: dynamic(
+    () =>
+      import(/* webpackChunkName: 'videoPlayer' */ './section/SectionVideoBg')
+  ),
+  player: dynamic(
+    () => import(/* webpackChunkName: 'videoPlayer' */ './player/Player')
+  ),
+  timeline_item: dynamic(
+    () => import(/* webpackChunkName: 'addon' */ './timeline/TimelineItem')
+  ),
+  timeline: dynamic(
+    () => import(/* webpackChunkName: 'addon' */ './timeline/Timeline')
+  ),
+  tabs: dynamic(() => import(/* webpackChunkName: 'slider' */ './tabs/Tabs')),
+  hubspot_meeting: dynamic(
+    () =>
+      import(
+        /* webpackChunkName: 'hubspot' */ './hubspot-meeting/HubspotMeeting'
+      )
+  ),
+  iframe: dynamic(
+    () => import(/* webpackChunkName: 'iframe' */ './iframe/Iframe')
+  ),
 
-LmCoreComponents.snackbar = dynamic(
-  () => import(/* webpackChunkName: 'snackbar' */ './snackbar/Snackbar')
-)
-LmCoreComponents.image_list = dynamic(
-  () => import(/* webpackChunkName: 'slider' */ './image-list/ImageList')
-)
-LmCoreComponents.image_list_item = dynamic(
-  () => import(/* webpackChunkName: 'slider' */ './image-list/ImageListItem')
-)
-LmCoreComponents.slider = dynamic(
-  () => import(/* webpackChunkName: 'slider' */ './slider/Slider')
-)
-LmCoreComponents.section_video_bg = dynamic(
-  () => import(/* webpackChunkName: 'videoPlayer' */ './section/SectionVideoBg')
-)
-LmCoreComponents.player = dynamic(
-  () => import(/* webpackChunkName: 'videoPlayer' */ './player/Player')
-)
-LmCoreComponents.timeline_item = dynamic(
-  () => import(/* webpackChunkName: 'addon' */ './timeline/TimelineItem')
-)
-LmCoreComponents.timeline = dynamic(
-  () => import(/* webpackChunkName: 'addon' */ './timeline/Timeline')
-)
-LmCoreComponents.tabs = dynamic(
-  () => import(/* webpackChunkName: 'slider' */ './tabs/Tabs')
-)
-LmCoreComponents.hubspot_meeting = dynamic(
-  () =>
-    import(/* webpackChunkName: 'hubspot' */ './hubspot-meeting/HubspotMeeting')
-)
-LmCoreComponents.iframe = dynamic(
-  () => import(/* webpackChunkName: 'iframe' */ './iframe/Iframe')
-)
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-LmCoreComponents.iframe_advanced = LmCoreComponents.iframe
-LmCoreComponents.chat_tawkto = dynamic(
-  () => import(/* webpackChunkName: 'tawkto' */ './chat/TawktoChatButton')
-)
-LmCoreComponents.chat_facebook = dynamic(
-  () =>
-    import(/* webpackChunkName: 'facebookChat' */ './chat/FacebookChatButton')
-)
-
-LmCoreComponents.instagram_post = dynamic(
-  () => import(/* webpackChunkName: 'instagram' */ './instagram/InstagramPost')
-)
-LmCoreComponents.instagram_list = dynamic(
-  () => import(/* webpackChunkName: 'instagram' */ './instagram/InstagramList')
-)
+  iframe_advanced: dynamic(
+    () => import(/* webpackChunkName: 'iframe' */ './iframe/Iframe')
+  ),
+  chat_tawkto: dynamic(
+    () => import(/* webpackChunkName: 'tawkto' */ './chat/TawktoChatButton')
+  ),
+  chat_facebook: dynamic(
+    () =>
+      import(/* webpackChunkName: 'facebookChat' */ './chat/FacebookChatButton')
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import { LmCoreComponents } from '..'
 import { LmGoogleForm } from '../components/google-form/GoogleForm'
+import { LmLazyComponents } from '../components/LazyNamedComponents'
 
+Object.keys(LmLazyComponents).forEach(
+  (k) => (LmCoreComponents[k] = LmLazyComponents[k])
+)
 LmCoreComponents.form = LmGoogleForm
 export { LmDefaultApp as default, reportWebVitals } from '..'
