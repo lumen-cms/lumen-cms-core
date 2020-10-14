98b5102d9202929616716f49b6fbc62078721f97
Dominic Garms <djgarms@gmail.com>
2020-10-14 15:58:04 +0800

    fix missing image src

--- src/utils/ImageService.ts
+++ src/utils/ImageService.ts
@@ -7,6 +7,9 @@ const boundCoordinate = (value: number, upperBound: number) => {
 }
 
 export function getOriginalImageDimensions(src: string) {
+  if (!src) {
+    return { width: 0, height: 0 }
+  }
   const splitted = src.split('/')
   const [originalWidth, originalHeight] = splitted[splitted.length - 3].split(
     'x'
