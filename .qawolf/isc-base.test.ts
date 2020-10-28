0b8bd4e895948897a1811515f477c5907b883fcf
Steffen Baer <stbaer.mail@gmail.com>
2020-10-28 10:07:03 +0800

    qawolf isc wip

--- .qawolf/isc-base.test.ts
+++ .qawolf/isc-base.test.ts
@@ -7,6 +7,7 @@ let context: BrowserContext;
 beforeAll(async () => {
   browser = await qawolf.launch();
   context = await browser.newContext();
+  // @ts-ignore
   await qawolf.register(context);
 });
 
@@ -17,22 +18,38 @@ afterAll(async () => {
 
 test("isc-base", async () => {
   const page = await context.newPage();
-  await page.goto("https://insidesoccercoaching.de/", { waitUntil: "domcontentloaded" });
-  await page.click(".MuiButton-outlined");
-  const page2 = await qawolf.waitForPage(context, 1, { waitUntil: "domcontentloaded" });
-  await page2.click('[aria-label="Email"]');
-  await page2.fill('[aria-label="Email"]', "stbaer.maillgmail.com");
-  await page2.press('[aria-label="Email"]', "Tab");
-  await page2.fill('[aria-label="Password"]', "A12345b!");
-  await page2.click('[aria-label="Log In"]');
-  await page2.click('[aria-label="Email"]');
-  await page2.fill('[aria-label="Email"]', "stbaer.mail@gmail.com");
-  await page2.click('[aria-label="Log In"]');
-  await qawolf.scroll(page2, "html", { x: 0, y: 383 });
-  await page2.click('text="Jetzt für $24.95 Kaufen"');
-  // const frame = await (await page2.waitForSelector('[name$="-frame"]')).contentFrame();
+  await page.goto("https://app.insidesoccercoaching.com/", { waitUntil: "domcontentloaded" });
+  // await page.click(".MuiButton-outlined");
+  // @ts-ignore
+  // const page2 = await qawolf.waitForPage(context, 1, { waitUntil: "domcontentloaded" });
+  await page.click('[aria-label="Email"]');
+  await page.fill('[aria-label="Email"]', "stbaer.maillgmail.com");
+  await page.press('[aria-label="Email"]', "Tab");
+  await page.fill('[aria-label="Password"]', "A12345b!");
+  await page.click('[aria-label="Log In"]');
+  await page.click('[aria-label="Email"]');
+  await page.fill('[aria-label="Email"]', "stbaer.mail@gmail.com");
+  await page.click('[aria-label="Log In"]');
+  // await qawolf.scroll(page, "html", { x: 0, y: 383 });
+  // await page.click('text="Jetzt für $24.95 Kaufen"');
+  // const frame = await (await page.waitForSelector('[name$="-frame"]')).contentFrame();
   // await frame.click("#contact-email");
   await setTimeout(() => {}, 3000)
+  const page2 = await qawolf.waitForPage(context, 0, { waitUntil: "domcontentloaded" });
+  await qawolf.scroll(page, "html", { x: 0, y: 413 });
+  await page2.click(".MuiButton-contained");
+  const frame = await (await page2.waitForSelector('[name$="-frame"]')).contentFrame();
+  if(frame) {
+    await frame.click("#contact-email");
+    await frame.fill("#contact-email", "test@test");
+    await frame.press("#contact-email", "Tab");
+    await frame.click("#contact-first-name");
+    await frame.fill("#contact-first-name", "A");
+    await frame.click("#contact-last-name");
+    await frame.fill("#contact-last-name", "B");
+    await frame.click("#card-number");
+    await frame.fill("#card-number", "4242 4242 4242 4242");
+  }
   // await frame.click("#card-number");
   // await frame.fill("#card-number", "4242 4242 4242 4242");
   // await frame.click("#card-expire-month");
@@ -40,7 +57,7 @@ test("isc-base", async () => {
   // await frame.press("#card-expire-month", "Tab");
   // await frame.fill("#card-expire-year", "22");
   // await frame.click("#contact-email");
-  // await frame.fill("#contact-email", "test@test");
+  // await frame.fill("#contact-email", "test@test.test");
   // await frame.click("#card-security");
   // await frame.click("#contact-first-name");
   // await frame.fill("#contact-first-name", "test");
---
+++ .qawolf/isc.test.ts
@@ -0,0 +1,31 @@
+import { Browser, BrowserContext } from "playwright";
+import qawolf from "qawolf";
+
+let browser: Browser;
+let context: BrowserContext;
+
+beforeAll(async () => {
+  browser = await qawolf.launch();
+  context = await browser.newContext();
+  await qawolf.register(context);
+});
+
+afterAll(async () => {
+  await qawolf.stopVideos();
+  await browser.close();
+});
+
+test("isc", async () => {
+  const page = await context.newPage();
+  await page.goto("https://dev-36d8hyhi.eu.auth0.com/login?state=g6Fo2SB1ZkpYbTBKM0lxdE1NZG1sRWhpUTlKSHZvVW8xZzFuSaN0aWTZIFJ6RFUyeC1nTEZzZlhZcU5kOUdJRUdFMEs0WU1Ib3BJo2NpZNkgTHdId0JldU1rZ3BGZm85MEZXRlRBZmtuUkx2RlAwQlk&client=LwHwBeuMkgpFfo90FWFTAfknRLvFP0BY&protocol=oauth2&scope=openid%20profile%20email%20read%3Ausers%20read%3Ausers_app_metadata&response_type=code&redirect_uri=https%3A%2F%2Fapp.insidesoccercoaching.com%2Fapi%2Fauth0%2Fcallback&auth0Client=eyJuYW1lIjoibmV4dGpzLWF1dGgwIiwidmVyc2lvbiI6IjAuMTYuMCJ9", { waitUntil: "domcontentloaded" });
+  await page.click('[aria-label="Email"]');
+  await page.fill('[aria-label="Email"]', "stbaer.mail@gmail.com");
+  await page.press('[aria-label="Email"]', "Tab");
+  await page.fill('[aria-label="Password"]', "A12345b!");
+  await page.click('[aria-label="Log In"]');
+  // await qawolf.scroll(page, "html", { x: 0, y: 527 });
+  await page.click(".MuiButton-contained");
+  const frame = await (await page.waitForSelector('[name$="-frame"]')).contentFrame();
+  await frame.click("#contact-email");
+  await frame.fill("#contact-email", "test@test.test");
+});
