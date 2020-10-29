bc0edb802d9a7427004e8c2839c581a786d33d17
Steffen Baer <stbaer.mail@gmail.com>
2020-10-29 13:01:19 +0800

    img minor change

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -40,25 +40,25 @@ export default function LmImage({
   const imageSource = content.source
   const storyblokImage = imageSource?.replace('//a', 'https://img2')
   const originalDimensions = getOriginalImageDimensions(imageSource || '')
-
   const square =
     property.includes('rounded-circle') || property.includes('square')
   const squareSize = square ? definedHeight || definedWidth || 120 : undefined
+  let maxWidth = definedWidth
 
-  // const imageAttrs = imageSource
-  //   ? getImageAttrs({
-  //       originalSource: imageSource,
-  //       width: squareSize || originalDimensions.width,
-  //       height: squareSize || originalDimensions.height,
-  //       focalPoint: content.focal_point,
-  //       smart: !!squareSize
-  //     })
-  //   : undefined
+  const imageAttrs = imageSource
+    ? getImageAttrs({
+        originalSource: imageSource,
+        width: squareSize || originalDimensions.width,
+        height: squareSize || originalDimensions.height,
+        focalPoint: content.focal_point,
+        smart: !!squareSize
+      })
+    : undefined
 
   if (!imageSource) {
     return <div /> // don't need to render anything
   }
-  // console.log(imageAttrs?.src)
+  console.log(imageAttrs?.src, property)
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions,jsx-a11y/no-static-element-interactions
     <div
@@ -73,10 +73,10 @@ export default function LmImage({
     >
       {storyblokImage && (
         <Image
-          src={storyblokImage}
+          src={imageAttrs?.src || storyblokImage}
           alt={content.alt || 'website image'}
-          width={originalDimensions.width}
-          height={originalDimensions.height}
+          width={squareSize || originalDimensions.width}
+          height={squareSize || originalDimensions.height}
           style={{
             cursor: onClick ? 'pointer' : undefined,
             maxHeight: 'inherit'
