5df1eff974ed6464a7479035af2af6632afc10ef
Dominic Garms <djgarms@gmail.com>
2020-11-02 17:08:09 +0800

    fix refetch route

--- src/pages/refetch.tsx
+++ src/pages/refetch.tsx
@@ -1,13 +1,33 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 import { getBaseProps } from '../utils/initial-props/getBaseProps'
 
 export default function Refetch() {
+  const { loginWithRedirect } = useAuth0()
+  const ref = useRef<HTMLButtonElement>(null)
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.location.reload()
+    if (ref?.current) {
+      ref.current.click()
     }
-  }, [])
-  return <div />
+  }, [ref])
+  return (
+    <div>
+      <button
+        type="button"
+        onClick={() =>
+          loginWithRedirect({
+            returnTo: window.location.origin
+          })
+        }
+        ref={ref}
+        style={{
+          display: 'none'
+        }}
+      >
+        logout..
+      </button>
+    </div>
+  )
 }
 
 export const getStaticProps = () => {
