c8ffa5a08dfa6f1202ec87f0f5370691bfebe03d
Dominic Garms <djgarms@gmail.com>
2020-11-03 10:39:10 +0800

    make changes to auth page

--- src/components/pages/Auth0Page.tsx
+++ src/components/pages/Auth0Page.tsx
@@ -2,10 +2,15 @@ import React, { useEffect } from 'react'
 import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react'
 import Error from 'next/error'
 import { useRouter } from 'next/router'
-import { LmDefaultPage, LmPagesIndexProps } from './DefaultPage'
+import { LmComponentRender } from '@LmComponentRender'
+import { LmPagesIndexProps } from './DefaultPage'
 import { hasAuth0PathCredentials } from '../../utils/auth0/auth0Helpers'
+import { AppSeo } from '../layout/AppSeo'
+import Layout from '../layout/Layout'
+import { NotFound } from './404'
 
 function MyPage(props: LmPagesIndexProps) {
+  const { settings, page, locale } = props
   const { asPath, replace } = useRouter()
   const { error, isLoading, user } = useAuth0()
 
@@ -14,14 +19,34 @@ function MyPage(props: LmPagesIndexProps) {
       replace('/')
     }
   }, [asPath, user, replace])
-  if (isLoading) {
-    return <h3>loading...</h3>
-  }
-  if (error) {
-    return <Error statusCode={500} title={error.message} />
+
+  if (error || props.error || !settings) {
+    return (
+      <Error
+        statusCode={500}
+        title={error?.message || 'Error occured or no settings found'}
+      />
+    )
   }
 
-  return <LmDefaultPage {...props} />
+  return (
+    <>
+      <AppSeo
+        settings={settings}
+        page={page}
+        previewImage={page?.preview_image}
+      />
+      <Layout settings={settings}>
+        {isLoading ? (
+          <h3>loading...</h3>
+        ) : page ? (
+          <LmComponentRender content={page} />
+        ) : (
+          <NotFound locale={locale} statusCode={404} />
+        )}
+      </Layout>
+    </>
+  )
 }
 
 export const Auth0Page = withAuthenticationRequired(MyPage)
