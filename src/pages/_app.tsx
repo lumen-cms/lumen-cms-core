3d68b57b2ae5b8100bfeb76ce5dc4e4c0a870560
Dominic Garms <djgarms@gmail.com>
2020-11-04 14:53:29 +0800

    comment default pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import { CONFIG } from '@CONFIG'
 // import { LmFastSpringProvider } from '../components/ecommerce/fastspring/LmFastspringProvider'
-export { Auth0App as default } from '../components/pages/_appAuth0'
+// export { Auth0App as default } from '../components/pages/_appAuth0'
+export { LmDefaultApp as default } from '../components/pages/_appDefault'
 
-// @ts-ignore
-// LmDefaultApp.getInitialProps = async (appContext) => {
-//   // calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(appContext)
-//
-//   return { ...appProps }
-// }
 // export default LmDefaultApp
 // if (process.env.NEXT_PUBLIC_AUTH_API_ASSIGN_ROLE) {
 //   LmCoreComponents.lm_app_providers.push(LmFastSpringProvider)
