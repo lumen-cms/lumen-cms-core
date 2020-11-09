1a581e312dc356d8463ba8332e8e42b97ac4378c
Dominic Garms <djgarms@gmail.com>
2020-11-09 23:11:48 +0800

    fix colors

--- src/components/pages/_appDefault.tsx
+++ src/components/pages/_appDefault.tsx
@@ -10,8 +10,8 @@ export function reportWebVitals({
   name,
   label
 }: NextWebVitalsMetric) {
-  gtag &&
-    gtag('event', name, {
+  window.gtag &&
+    window.gtag('event', name, {
       event_category:
         label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
       value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
