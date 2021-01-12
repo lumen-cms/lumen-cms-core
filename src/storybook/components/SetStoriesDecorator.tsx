12e1a45b5734312ed0c1380bc0164a011e94668b
Dominic Garms <djgarms@gmail.com>
2021-01-12 16:23:21 +0800

    update pkg

--- src/storybook/components/SetStoriesDecorator.tsx
+++ src/storybook/components/SetStoriesDecorator.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react'
-import { useEffect, useState } from 'react'
-import { createGlobalState } from 'react-hooks-global-state'
+import { useState } from 'react'
 import { Story as StoryType } from '@storybook/react/types-6-0.d'
 import { AppContextProps } from '@context/AppContext'
 import { LmComponentRender } from '@LmComponentRender'
 import AppProvider from '../../components/provider/AppProvider'
 
-interface StorybookState {
-  allTags: { value: string; label: string }[]
-}
+// todo...
+// interface StorybookState {
+//   allTags: { value: string; label: string }[]
+// }
 
-const storybookDefault: StorybookState = {
-  allTags: []
-}
-export const {
-  setGlobalState,
-  useGlobalState,
-  getGlobalState
-} = createGlobalState(storybookDefault)
+// const storybookDefault: StorybookState = {
+//   allTags: []
+// }
+// export const {
+//   setGlobalState,
+//   useGlobalState,
+//   getGlobalState
+// } = createGlobalState(storybookDefault)
 
 const SetStoriesDecorator = (Story: StoryType) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -28,57 +28,57 @@ const SetStoriesDecorator = (Story: StoryType) => {
   // @ts-ignore
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [values, setValues] = useState<AppContextProps>()
-  const [, setAllTags] = useGlobalState('allTags')
-  useEffect(() => {
-    const fetch = async () => {
-      // const [categories, stories, tags] = await Promise.all([
-      // TODO
-      // LmStoryblokService.getAll('cdn/stories', {
-      //   per_page: 100,
-      //   sort_by: 'content.name:asc',
-      //   filter_query: {
-      //     component: {
-      //       in: 'category'
-      //     }
-      //   }
-      // }),
-      // LmStoryblokService.getAll(`cdn/stories`, {
-      //   per_page: 100,
-      //   excluding_fields:
-      //     'body,meta_robots,property,meta_title,meta_description,seo_body',
-      //   sort_by: 'published_at:desc',
-      //   filter_query: {
-      //     component: {
-      //       in: 'page'
-      //     }
-      //   }
-      // }),
-      // ])
-      // const tagList = []
-      // (tags &&
-      //   tags.data?.tags &&
-      //   tags.data?.tags.map(
-      //     (item: { name: string; taggings_count: number }) => ({
-      //       value: item.name,
-      //       label: `${item.name} (${item.taggings_count})`
-      //     })
-      //   )) ||
-      // []
-      // const stories = []
-      // const categories = []
-      // setAllTags(tagList)
-      // setLoaded(true)
-      // setValues({
-      //   listWidgetData: {
-      //     storyblok_list: stories
-      //   },
-      //   allCategories: categories,
-      //   allStaticContent: []
-      // })
-    }
-
-    fetch()
-  }, [setAllTags])
+  // const [, setAllTags] = useGlobalState('allTags')
+  // useEffect(() => {
+  //   const fetch = async () => {
+  // const [categories, stories, tags] = await Promise.all([
+  // TODO
+  // LmStoryblokService.getAll('cdn/stories', {
+  //   per_page: 100,
+  //   sort_by: 'content.name:asc',
+  //   filter_query: {
+  //     component: {
+  //       in: 'category'
+  //     }
+  //   }
+  // }),
+  // LmStoryblokService.getAll(`cdn/stories`, {
+  //   per_page: 100,
+  //   excluding_fields:
+  //     'body,meta_robots,property,meta_title,meta_description,seo_body',
+  //   sort_by: 'published_at:desc',
+  //   filter_query: {
+  //     component: {
+  //       in: 'page'
+  //     }
+  //   }
+  // }),
+  // ])
+  // const tagList = []
+  // (tags &&
+  //   tags.data?.tags &&
+  //   tags.data?.tags.map(
+  //     (item: { name: string; taggings_count: number }) => ({
+  //       value: item.name,
+  //       label: `${item.name} (${item.taggings_count})`
+  //     })
+  //   )) ||
+  // []
+  // const stories = []
+  // const categories = []
+  // setAllTags(tagList)
+  // setLoaded(true)
+  // setValues({
+  //   listWidgetData: {
+  //     storyblok_list: stories
+  //   },
+  //   allCategories: categories,
+  //   allStaticContent: []
+  // })
+  //   }
+  //
+  //   fetch()
+  // }, [setAllTags])
   if (loaded && values) {
     return (
       <AppProvider
