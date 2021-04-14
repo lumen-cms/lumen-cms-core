c4d360608ddb1f9a737a7705a84c1311c6a38635
Dominic Garms <djgarms@gmail.com>
2021-04-14 14:34:05 +0800

    minor change

--- src/components/accordion/Accordion.stories.tsx
+++ src/components/accordion/Accordion.stories.tsx
@@ -1,28 +1,56 @@
 import { LmComponentRender } from '@LmComponentRender'
+import { Meta, Story } from '@storybook/react'
 import {
   storyAccordion,
   storyAccordionItem
 } from '../../storybook/core/various'
 import { get3ColumnsSection } from '../../storybook/section'
+import LmAccordion from './Accordion'
+import { LmAccordionProps } from './accordionTypes'
 
 export default {
-  title: 'Design/Surfaces/Accordion'
+  title: 'Design/Surfaces/Accordion',
+  component: LmAccordion
+} as Meta
+
+const Template: Story<LmAccordionProps> = (args) => <LmAccordion {...args} />
+
+const blok = {
+  ...storyAccordion(),
+  body: [
+    {
+      ...storyAccordionItem({ count: 1 }),
+      body: [get3ColumnsSection({ count: 1, knob: 'Column Content' })]
+    },
+    {
+      ...storyAccordionItem({ count: 2 }),
+      body: [get3ColumnsSection({ count: 2, knob: 'Column Content' })]
+    }
+  ]
 }
 
-export const Playground = () => {
-  const blok = {
-    ...storyAccordion(),
+export const Basic = Template.bind({})
+Basic.args = {
+  content: {
+    _uid: '2342',
+    component: 'accordion',
     body: [
       {
-        ...storyAccordionItem({ count: 1 }),
-        body: [get3ColumnsSection({ count: 1, knob: 'Column Content' })]
-      },
-      {
-        ...storyAccordionItem({ count: 2 }),
-        body: [get3ColumnsSection({ count: 2, knob: 'Column Content' })]
+        _uid: '3123',
+        component: 'accordion_item',
+        title: 'Test',
+        body: [
+          {
+            _uid: '23123',
+            component: 'headline',
+            text: 'some test'
+          }
+        ]
       }
     ]
   }
+}
 
+export const Playground = () => {
   return <LmComponentRender content={blok} />
 }
