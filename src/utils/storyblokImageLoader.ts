1a0393dbf16c21302eda67707fac6e845d41a1bd
Dominic Garms <djgarms@gmail.com>
2021-11-26 09:34:53 +0800

    minor change

--- src/utils/storyblokImageLoader.ts
+++ src/utils/storyblokImageLoader.ts
@@ -4,14 +4,14 @@ export const loader = ({ src, width, quality }: ImageLoaderProps) => {
   const originalPath = src.replace(/^(https?:)?\/\/a.storyblok.com\//, '')
   let opts = ''
   if (width) {
-    opts += `${width}x0`
+    opts += `/${width}x0`
   }
 
   opts += `/filters:quality(${
     quality || process.env.NEXT_PUBLIC_IMAGE_QUALITY || 75
   })`
 
-  return `https://a.storyblok.com/${originalPath}/m/${opts}`
+  return `https://a.storyblok.com/${originalPath}/m${opts}`
 }
 
 export const storyblokImageLoader = (src: string | undefined) =>
