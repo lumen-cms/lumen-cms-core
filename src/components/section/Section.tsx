b5f9514d22fb44ebd38e63fdd3665b0c4fa5dd39
Dominic Garms <djgarms@gmail.com>
2021-12-06 11:41:18 +0800

    update forms to work on dark sections

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -22,9 +22,17 @@ const useStyles = makeStyles({
     }
   },
   dark: {
-    '& .MuiButton-root.lm-default-color, & .MuiIconButton-root.lm-default-color':
+    '& .MuiButton-root.lm-default-color, & .MuiIconButton-root.lm-default-color, & .MuiInputBase-root, & .MuiFormLabel-root':
       {
         color: 'inherit',
+        '&.MuiInput-underline:before, &.MuiFilledInput-underline:before, &.MuiFilledInput-underline:after':
+          {
+            borderBottom: '1px solid currentColor'
+          },
+        '& .MuiOutlinedInput-notchedOutline, &:hover .MuiOutlinedInput-notchedOutline':
+          {
+            borderColor: 'currentColor'
+          },
         '&.MuiButton-outlined,&.lm-outlined': {
           borderColor: 'currentColor'
         }
