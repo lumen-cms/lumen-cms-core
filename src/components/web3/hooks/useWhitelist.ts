faf9cdbd3f9cd1223e93a7a1d302ee66511fe3a8
Dominic Garms <djgarms@gmail.com>
2022-05-31 12:13:44 +0800

    fix url params

--- src/components/web3/hooks/useWhitelist.ts
+++ src/components/web3/hooks/useWhitelist.ts
@@ -3,24 +3,31 @@ import useSWR from 'swr'
 import { useWeb3React } from '@web3-react/core'
 import { CHAINS } from '../chainsConfig'
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
+const fetcher = (account: string, chainId: number, contractToken: string) => {
+  const url = `${
+    process.env.STORYBOOK ? 'http://localhost:3000' : ''
+  }/api/sign/message`
+  const params = new URLSearchParams()
+  params.append('account', account)
+  params.append('chainId', `${chainId}`)
+  params.append('contractAddress', contractToken)
+  const fullPath = url + '?' + params.toString()
+  return fetch(fullPath).then((res) => res.json())
+}
 
 export default function useWhitelist(content: MoralisMintProps['content']) {
   const { account, chainId, error } = useWeb3React()
   const selectedChain = CHAINS[content.chain || 'mainnet']
   const isCorrectChain = selectedChain?.id === chainId
 
-  const url = `${
-    process.env.STORYBOOK ? 'http://localhost:3000' : ''
-  }/api/sign/${account}?chainId=${chainId}&contractAddress=${
-    content.contract_token
-  }`
   const {
     error: whitelistError,
     isValidating,
     data
   } = useSWR<{ signed: string; amount: number }>(
-    content.sale === 'whitelist' && account && isCorrectChain ? url : null,
+    content.sale === 'whitelist' && account && isCorrectChain
+      ? [account, chainId, content.contract_token]
+      : null,
     fetcher,
     {
       revalidateOnFocus: false
