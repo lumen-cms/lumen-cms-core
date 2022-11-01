b9e03b9496320c1aff865aa8a54bd9a1b05afe02
Dominic Garms <djgarms@gmail.com>
2022-11-01 09:08:26 +0800

    fix parallax

--- src/components/section/SectionParallax.tsx
+++ src/components/section/SectionParallax.tsx
@@ -13,11 +13,103 @@ import { BannerLayer } from 'react-scroll-parallax/dist/components/ParallaxBanne
 export default function LmSectionParallax({
   content,
   sectionPosition
-}: LmSectionParallaxProps): JSX.Element {
+}: LmSectionParallaxProps) {
   const contentHeight = content.height
   const isPriority = sectionPosition === 0 || !!content.disable_lazy_load
-  console.log(content.elements)
-  return (
+
+  const Children =
+    content.elements?.map(
+      (
+        {
+          always_complete_animation,
+          amount,
+          speed,
+          image,
+          parallax_item_data,
+          children,
+          expanded,
+          easing,
+          ...props
+        },
+        index
+      ) => {
+        const layerProps: BannerLayer = {}
+        Object.keys(props).forEach((key) => {
+          const lProp = props[key].split(',').map((i: string) => {
+            const trimmed = i.trim()
+            return Number(trimmed) ? Number(trimmed) : trimmed
+          })
+
+          layerProps[key] = lProp
+        })
+        return (
+          <ParallaxBannerLayer
+            key={index}
+            shouldAlwaysCompleteAnimation={always_complete_animation || false}
+            speed={amount ? Number(amount) * 10 : speed ? Number(speed) : -20}
+            expanded={expanded}
+            easing={easing || undefined}
+            {...layerProps}
+          >
+            <>
+              {image && (
+                <Image
+                  {...getOriginalImageDimensions(image)}
+                  priority={isPriority}
+                  style={{
+                    width: '100%',
+                    height: 'auto'
+                  }}
+                  alt={'image of ' + image}
+                  {...storyblokImageLoader(image)}
+                  src={getRootImageUrl(image)}
+                  sizes={
+                    content.height && content.ratio
+                      ? `${content.height}px`
+                      : undefined
+                  }
+                  {...(parallax_item_data?.base64 && {
+                    placeholder: 'blur',
+                    blurDataURL: parallax_item_data.base64
+                  })}
+                />
+              )}
+              {children?.map((child) => (
+                <LmComponentRender content={child} key={child._uid} />
+              ))}
+            </>
+          </ParallaxBannerLayer>
+        )
+      }
+    ) ?? []
+  if (content.body?.length) {
+    Children.push(
+      <Box
+        key={Children.length + 1}
+        sx={{
+          overflowX: 'hidden',
+          zIndex: 1,
+          position: 'absolute',
+          width: '100%',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          height: '100%',
+          '& > .lm-grid-row__wrap': {
+            overflowX: 'hidden',
+            height: '100%'
+          }
+        }}
+        className={content.class_names?.values?.join(' ')}
+      >
+        {content.body.map((blok) => (
+          <LmComponentRender content={blok} key={blok._uid} />
+        ))}
+      </Box>
+    )
+  }
+  return Children.length ? (
     <Box
       position={'relative'}
       width={'100%'}
@@ -43,98 +135,8 @@ export default function LmSectionParallax({
           overflow: content.allow_overflow ? 'visible' : 'hidden'
         }}
       >
-        {content.elements?.map(
-          (
-            {
-              always_complete_animation,
-              amount,
-              speed,
-              image,
-              parallax_item_data,
-              children,
-              expanded,
-              easing,
-              ...props
-            },
-            index
-          ) => {
-            const layerProps: BannerLayer = {}
-            Object.keys(props).forEach((key) => {
-              const lProp = props[key].split(',').map((i: string) => {
-                const trimmed = i.trim()
-                return Number(trimmed) ? Number(trimmed) : trimmed
-              })
-
-              layerProps[key] = lProp
-            })
-            return (
-              <ParallaxBannerLayer
-                key={index}
-                shouldAlwaysCompleteAnimation={
-                  always_complete_animation || false
-                }
-                speed={
-                  amount ? Number(amount) * 10 : speed ? Number(speed) : -20
-                }
-                expanded={expanded}
-                easing={easing || undefined}
-                {...layerProps}
-              >
-                <>
-                  {image && (
-                    <Image
-                      {...getOriginalImageDimensions(image)}
-                      priority={isPriority}
-                      style={{
-                        width: '100%',
-                        height: 'auto'
-                      }}
-                      {...storyblokImageLoader(image)}
-                      src={getRootImageUrl(image)}
-                      sizes={
-                        content.height && content.ratio
-                          ? `${content.height}px`
-                          : undefined
-                      }
-                      {...(parallax_item_data?.base64 && {
-                        placeholder: 'blur',
-                        blurDataURL: parallax_item_data.base64
-                      })}
-                    />
-                  )}
-                  {children?.map((child) => (
-                    <LmComponentRender content={child} key={child._uid} />
-                  ))}
-                </>
-              </ParallaxBannerLayer>
-            )
-          }
-        )}
-        {!!content.body?.length && (
-          <Box
-            sx={{
-              overflowX: 'hidden',
-              zIndex: 1,
-              position: 'absolute',
-              width: '100%',
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              height: '100%',
-              '& > .lm-grid-row__wrap': {
-                overflowX: 'hidden',
-                height: '100%'
-              }
-            }}
-            className={content.class_names?.values?.join(' ')}
-          >
-            {content.body.map((blok) => (
-              <LmComponentRender content={blok} key={blok._uid} />
-            ))}
-          </Box>
-        )}
+        {Children}
       </ParallaxBanner>
     </Box>
-  )
+  ) : null
 }
