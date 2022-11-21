03a35defcf7e66b4c8f19b625f828164e8ede876
Dominic Garms <djgarms@gmail.com>
2022-11-21 14:50:51 +0800

    update

--- src/pages/refetch.tsx
+++ src/pages/refetch.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { getBaseProps } from '../utils/initial-props/getBaseProps'
 
-export default function Lmrefetch() {
+export default function Refetch() {
   const { loginWithRedirect } = useAuth0()
   const ref = useRef<HTMLButtonElement>(null)
   useEffect(() => {
